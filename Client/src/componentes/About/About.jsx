688375c5736685cd306a9d3e0eab49873c39283a
GGruszczyk <germangruszczyk@gmail.com>
2023-05-04 18:40:41 -0300

    Mensaje de confirmación

---
+++ Client/src/componentes/About/About.jsx
@@ -0,0 +1,10 @@
+import style from './About.module.css'
+const About = () => {
+    return (
+        <>
+        <h2 className={style.title}>About...Ssasaaassaaa</h2>
+        </>
+    )
+}
+
+export default About;
